41fbb975d28f68cb0c27f017c2498219d48bffbb
Ghithub <gnewsgroup@gmail.com>
2017-02-07 14:12:35 -0500

    Attempted to configure the application to use google authentication service. Not working yet.

--- Global.asax.cs
+++ Global.asax.cs
@@ -12,12 +12,10 @@ namespace MvcApplication1 {
     public class MvcApplication : System.Web.HttpApplication {
         protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
+            BundleConfig.RegisterBundles(BundleTable.Bundles);            
             ModelBinders.Binders.Add(typeof(Property), new PropertyRegistrationModelBinder());
             ModelBinders.Binders.Add(typeof(Agent), new PropertyRegistrationModelBinder());
         }
